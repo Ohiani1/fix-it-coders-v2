9970d9726cc56b2c0b5301ddab6c506d8b78fe77
Ohiani1 <ohianiabdulkadir7@gmail.com>
2024-09-14 01:12:50 -0500

    refactor

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,7 +44,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <Image
-            src={"/logo.png"}
+            src={"public/logo.png"}
             width={90}
             height={90}
             alt="fix-it-coders logo"
