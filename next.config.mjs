a67593aa2455712f2a2250a9096d29c67534b7d8
Ohiani1 <ohianiabdulkadir7@gmail.com>
2024-09-13 22:38:42 -0500

    configured next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 import {withSentryConfig} from '@sentry/nextjs';
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
 
 export default withSentryConfig(withSentryConfig(nextConfig, {
 // For all available options, see:
